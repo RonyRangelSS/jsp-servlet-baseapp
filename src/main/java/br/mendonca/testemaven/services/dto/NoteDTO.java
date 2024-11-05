deea2090464fb6d81a627b94c24c71cebed91afa
Michael Albuquerque <michael.albqq@gmail.com>
2024-11-05 15:21:34 -0300

    feat: criando um DTO para a entity note

---
+++ src/main/java/br/mendonca/testemaven/services/dto/NoteDTO.java
@@ -0,0 +1,72 @@
+package br.mendonca.testemaven.services.dto;
+
+import br.mendonca.testemaven.model.entities.Note;
+
+public class NoteDTO {
+    private String uuid;
+    private String userId;
+    private String noteTitle;
+    private String noteContent;
+    private int date;
+    private boolean isDone;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    public static NoteDTO noteMapper(Note note) {
+        NoteDTO dto = new NoteDTO();
+        dto.setUuid(note.getUuid());
+        dto.setUserId(note.getUserId());
+        dto.setNoteTitle(note.getNoteTitle());
+        dto.setNoteContent(note.getNoteContent());
+        dto.setDate(note.getDate());
+        dto.setDone(note.isDone());
+
+        return dto;
+    }
+}
