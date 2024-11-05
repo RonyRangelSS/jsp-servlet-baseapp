f226024099965b19e7db0f631968294ddaea8b86
RonyRangelSS <rony.rangel@upe.br>
2024-11-05 14:39:47 -0300

    Criação da entidade task

---
+++ src/main/java/br/mendonca/testemaven/model/entities/Task.java
@@ -0,0 +1,43 @@
+package br.mendonca.testemaven.model.entities;
+
+public class Task {
+
+    private String uuid;
+    private String taskName;
+    private Boolean isCompleted;
+    private Integer priority;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Boolean getCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(Boolean completed) {
+        isCompleted = completed;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+
+}
