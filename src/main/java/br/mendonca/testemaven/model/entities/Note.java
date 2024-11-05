c82d5213c13fa33f8be1333cfb6b43cc01da6d51
Michael Albuquerque <michael.albqq@gmail.com>
2024-11-05 15:00:31 -0300

    feat: adicionando uma classe note em entities

---
+++ src/main/java/br/mendonca/testemaven/model/entities/Note.java
@@ -0,0 +1,59 @@
+package br.mendonca.testemaven.model.entities;
+
+public class Note {
+    private String uuid;
+    private String userId;
+    private String noteTitle;
+    private String noteContent;
+    private int date;
+    private boolean isDone;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+}
+
