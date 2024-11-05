8a58c5844528e501228c2129cbeb20c7819ec9c9
RonyRangelSS <rony.rangel@upe.br>
2024-11-05 14:51:07 -0300

    adição do taskDTO

---
+++ src/main/java/br/mendonca/testemaven/services/dto/TaskDTO.java
@@ -0,0 +1,53 @@
+package br.mendonca.testemaven.services.dto;
+
+import br.mendonca.testemaven.model.entities.Task;
+
+public class TaskDTO {
+
+    private String uuid;
+    private String taskName;
+    private Boolean isCompleted;
+    private Integer priority;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Boolean getCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(Boolean completed) {
+        isCompleted = completed;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public static TaskDTO taskMapper(Task task) {
+        TaskDTO dto = new TaskDTO();
+        dto.setUuid(task.getUuid());
+        dto.setTaskName(task.getTaskName());
+        dto.setPriority(task.getPriority());
+        dto.setCompleted(task.getCompleted());
+
+        return dto;
+    }
+}
